e7a19ce3f5941fa7fb639494dcf7e684c9834210
alainlou <loual@utschools.ca>
2017-12-04 20:35:54 -0500

    some changes

--- src/lou/alain/Driver.java
+++ src/lou/alain/Driver.java
@@ -1,5 +1,10 @@
 package lou.alain;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import twitter4j.GeoLocation;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -10,21 +15,29 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class Driver {
 	
-	static String [] keys = {};//get your own
+	static String [] keys = //get your own
 			
-	public static void main(String[] args) throws TwitterException {
+	public static void main(String[] args) throws TwitterException, FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter pw = new PrintWriter("data.txt", "UTF-8");
+		
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true).setOAuthConsumerKey(keys[0]).setOAuthConsumerSecret(keys[1]).setOAuthAccessToken(keys[2]).setOAuthAccessTokenSecret(keys[3]);
 		TwitterFactory tf = new TwitterFactory(cb.build());
 		Twitter t = tf.getInstance();
 		
-		
 		Query q =  new Query("#ClimateChange");
-		q.setCount(15);
+		//q.setGeoCode(new GeoLocation(43.653908, -79.384293), 1000000, Query.KILOMETERS);
+		
+		q.setCount(100);
 		QueryResult r = t.search(q);
 		for(Status s: r.getTweets()){
-			System.out.println(s);
+			System.out.println(s.getText());
+			//if(s.getGeoLocation() != null){
+				pw.println(s.getGeoLocation() + "\t" + s.getText());
+			//}
 		}
+		
+		pw.close();
 
 	}
 
